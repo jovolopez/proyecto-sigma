9cf66f2c3b8c1fceb8ab0e12298df15187d07c07
emiliaanoldz <45268293+emiliaanoldz@users.noreply.github.com>
2019-09-25 18:03:34 -0300

    footer php

---
+++ footer.php
@@ -0,0 +1,51 @@
+<html lang="es" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="css/footer.css">
+  </head>
+  <body>
+
+    <footer>
+      <div class="bg-cont">
+        <div class="izq">
+          <h3>Categorias.</h3>
+          <ul>
+            <li>Categoria 1</li>
+            <li>Categoria 2</li>
+            <li>Categoria 3</li>
+            <li>Categoria 4</li>
+            <li>Categoria 5</li>
+          </ul>
+        </div>
+
+        <div class="center">
+          <h3>Redes Sociales</h3>
+          <ul>
+            <li>Instagram</li>
+            <li>Facebook</li>
+            <li>YouTube</li>
+            <li>Twitter</li>
+          </ul>
+        </div>
+
+        <div class="der">
+          <form class="newsletter" action="index.html" method="post">
+            <label for="correo">Suscribase</label>
+            <br>
+            <input type="email" name="" value="" placeholder="correo@ejemplo.com">
+            <input class="button" type="submit" name="" value="Suscribirse">
+          </form>
+
+          <br>
+          <h3>Dónde estamos:</h3>
+          <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3283.3496434793474!2d-58.38383774949727!3d-34.620603565802206!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95bccb28ed147873%3A0xa6a660992184c8be!2sUnnamed%20Road%2C%20C1103%20CABA!5e0!3m2!1ses-419!2sar!4v1569443551766!5m2!1ses-419!2sar" width="300" height="150" frameborder="0" style="border:0;" allowfullscreen=""></iframe>
+        </div>
+      </div>
+      <div class="copyright">
+        <p>Sigma 2019, todos los derechos reservados.</p>
+      </div>
+    </footer>
+
+
+  </body>
+</html>
