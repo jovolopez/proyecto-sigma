89508196a5741cb7f23b4b28987d0ffedac5f64a
Emiliano <emivargas1998@gmail.com>
2019-09-26 18:50:49 -0300

    checkout init

--- partials/nav-menu.php
+++ partials/nav-menu.php
@@ -1,6 +1,11 @@
+<head>
+  <link rel="stylesheet" href="css/master.css">
+</head>
+<header>
 <div class="nav-menu">
   <div><a href="home.php"><ion-icon class="nav-icon" name="home"></ion-icon></a></div>
   <div><a href="list.php"><ion-icon class="nav-icon" name="list"></ion-icon></a></div>
   <div><a href="cart.php"><ion-icon class="nav-icon" name="cart"></ion-icon></a></div>
   <div><a href="perfil.php"><ion-icon class="nav-icon" name="person"></ion-icon></a></div>
 </div>
+<script src="https://unpkg.com/ionicons@4.5.10-0/dist/ionicons.js"></script>
