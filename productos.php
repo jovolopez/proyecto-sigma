51ed68aeef68c704eedffa7dcad4fd7f01006e97
EliecerGonzalez <eliecerega@gmail.com>
2019-09-27 17:33:36 -0300

    cambios en productos

--- productos.php
+++ productos.php
@@ -14,14 +14,31 @@
       <div class="productos">
         <article class="producto">
           <img src="img/macbook.jpg" alt="Macbook">
+
         </article>
       </div>
 
       <div class="descripcion">
-        <h2>Macbook Infinity 2019</h2>
+        <div class="descripcion-text">
+          <h2>Macbook Infinity 2019</h2>
+          <hr size="1px" color="#ddd">
+        </div>
+        <div class="precio">
+          <h2>Precio: 5000 U$D</h2>
+        </div>
       </div>
 
       <div class="list">
+
+        <h3>Caracteristicas</h3>
+        <hr size="1px" color="#ddd">
+
+        <ul>
+          <li>Procesador ts-edith de 4 nucleos y 2.4GHz</li>
+          <li>Almacenamiento 1T SSD y 32Gb ram DDR3</li>
+          <li>Graficos Intel Iris Plus Graphics 655</li>
+
+        </ul>
 
         </div>
     </main>
