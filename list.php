0f8b0ded50e5156b9c00415b65ef2471b677da86
Javveru <thezepols@gmail.com>
2019-10-07 14:34:52 -0300

    Sombra e iconos nav-bar y list actualizada.

--- list.php
+++ list.php
@@ -12,17 +12,39 @@
     <?php require_once("partials/header.php");?>
 
     <main class="container">
-      <div class="ordenar">
-        <div class="adsd">
-          <a href="home.php">
-            <ion-icon name="grid"></ion-icon>
-          </a>
+      <div class="opciones">
+        <div class="opcion">
+          <div class="ordenar">
+            <div class="vista-c">
+              <a href="home.php">
+                <ion-icon name="grid"></ion-icon>
+              </a>
+            </div>
+            <div class="barra">
+              |
+            </div>
+            <div class="vista-l">
+              <a href="list.php">
+                <ion-icon name="list-box"></ion-icon>
+              </a>
+            </div>
+          </div>
         </div>
-        <div class="">
-          <a href="list.php">
-            <ion-icon name="list-box"></ion-icon>
-          </a>
-
+        <div class="opcion">
+          <div class="opt-text">
+            Ordenar
+          </div>
+          <div class="opt-icon">
+            <ion-icon name="arrow-dropdown"></ion-icon>
+          </div>
+        </div>
+        <div class="opcion">
+          <div class="opt-text">
+            Filtrar
+          </div>
+          <div class="opt-icon">
+            <ion-icon name="options"></ion-icon>
+          </div>
         </div>
       </div>
       <section class="productos">
@@ -31,7 +53,7 @@
             <img src="img/macbook.jpg" alt="Macbook">
           </div>
           <div class="info-p">
-            <p>Macbook i5 OFERTA DEL AÑO</p>
+            <p class="titulo-p">Macbook i5 OFERTA DEL AÑO</p>
             <p><b>1303$ ARS</b> 20%off</p>
             <label for="">Buenos Aires</label>
           </div>
