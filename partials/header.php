275bb57641850371145ec539f3c6c3a294af6bc5
Javveru <thezepols@gmail.com>
2019-10-08 01:30:05 -0300

    Lista v1 + header desktop v1

--- partials/header.php
+++ partials/header.php
@@ -2,10 +2,64 @@
     <link rel="stylesheet" href="css/master.css">
   </head>
 <header>
-  <a href="home.php">
-    <ion-icon name="arrow-up" size="large" role="img" class="md icon-large hydrated" aria-label="arrow-up"></ion-icon>
-  </a>
-  <h1>Sigma</h1>
-  <ion-icon name="ios-search" size="large" role="img" class="md icon-large hydrated" aria-label="search"></ion-icon>
+  <div class="cont-head">
+    <div class="h-top">
+      <a href="#">
+        <ion-icon name="arrow-up" size="large" role="img" class="md icon-large hydrated" aria-label="arrow-up"></ion-icon>
+      </a>
+    </div>
+    <div class="h-logo">
+      <a href="home.php">
+        <h1>Sigma</h1>
+      </a>
+    </div>
+    <div class="h-busq-sm">
+      <ion-icon name="ios-search" size="large" role="img" class="md icon-large hydrated" aria-label="search"></ion-icon>
+    </div>
+    <div class="h-bar">
+      <div class="h-hiperv">
+        <a href="checkout.php">
+          <div class="h-icontext">
+            <div class="h-text">
+              Ver carrito
+            </div>
+            <div class="h-icon">
+              <ion-icon class="nav-icon" name="cart"></ion-icon>
+            </div>
+          </div>
+        </a>
+      </div>
+      <div class="h-hiperv">
+        <a href="perfil.php">
+          <div class="h-icontext">
+            <div class="h-text">
+              Mi perfil
+            </div>
+            <div class="h-icon">
+              <ion-icon class="nav-icon" name="person"></ion-icon>
+            </div>
+          </div>
+        </a>
+      </div>
+    </div>
+  </div>
+  <div class="cont-head-b">
+    <form class="form-busq" action="index.html" method="get">
+      <div class="h-categorias">
+        <div class="cate-text">
+          Categorías
+        </div>
+        <div class="cate-icon">
+          <ion-icon name="arrow-dropdown"></ion-icon>
+        </div>
+      </div>
+      <div class="busq-input">
+        <input type="text" name="search" value="" class="search">
+      </div>
+      <div class="busq-icon">
+        <ion-icon name="ios-search" role="img" aria-label="search"></ion-icon>
+      </div>
+    </form>
+  </div>
 </header>
 <script src="https://unpkg.com/ionicons@4.5.10-0/dist/ionicons.js"></script>
