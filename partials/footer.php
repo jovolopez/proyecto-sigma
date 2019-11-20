d49985cdc1be89e6f13b58b4a702403df2bcb764
DigitalHouse <you@example.com>
2019-11-20 17:51:26 -0300

    Redundancias -

--- partials/footer.php
+++ partials/footer.php
@@ -1,10 +1,4 @@
-<html lang="es" dir="ltr">
-  <head>
-    <link rel="stylesheet" href="css/footer.css">
-  </head>
-  <body>
-
-    <footer>
+<footer>
       <div class="bg-cont">
         <div class="izq">
           <h3>Categorias.</h3>
@@ -41,7 +35,7 @@
             <input type="email" name="" value="" placeholder="correo@ejemplo.com">
             <button type="submit" name="button">Suscribirse</button>
           </form>
-          
+
           <br>
           <h3>Dónde estamos:</h3>
           <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3283.3496434793474!2d-58.38383774949727!3d-34.620603565802206!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95bccb28ed147873%3A0xa6a660992184c8be!2sUnnamed%20Road%2C%20C1103%20CABA!5e0!3m2!1ses-419!2sar!4v1569443551766!5m2!1ses-419!2sar" width="300" height="150" frameborder="0" style="border:0;" allowfullscreen=""></iframe>
@@ -56,4 +50,3 @@
 
   <script src="https://unpkg.com/ionicons@4.5.10-0/dist/ionicons.js"></script>
   </body>
-</html>
